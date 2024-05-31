446ab4c629a64bfd9a394dc98da2533fe150a95b
lucasherlon <lucasherlondsmc@gmail.com>
2024-05-30 21:18:33 -0300

    creating the discord bot

---
+++ geminiapi/gemini_api.go
@@ -0,0 +1,61 @@
+package geminiapi
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+)
+
+// Generate the prompt to be translated
+func GeneratePrompt(input string) string {
+	erro := godotenv.Load(".env")
+
+	if erro != nil {
+		fmt.Println("Error loading .env file:", erro)
+	}
+	idiomaSaida := "português brasileiro"
+
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_KEY")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(client *genai.Client) {
+		err := client.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(client)
+
+	model := client.GenerativeModel("gemini-1.5-flash-latest")
+	fmt.Println(input)
+
+	prompt := fmt.Sprintf("Por gentileza, traduza o seguinte texto para o %v: %v", idiomaSaida, input)
+
+	resp, err := model.GenerateContent(ctx, genai.Text(prompt))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	response := showResponse(resp)
+	return response
+}
+
+// Show the response generatad from the model
+func showResponse(resp *genai.GenerateContentResponse) string {
+	var response string
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				response = response + fmt.Sprintf("%v", part)
+			}
+		}
+	}
+	return response
+}
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"traduz.ai/geminiapi"
+)
+
+func main() {
+	const command string = "!traduz"
+
+	godotenv.Load(".env")
+	token := os.Getenv("BOT_KEY")
+
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		args := strings.Split(m.Content, " ")
+		if args[0] != command {
+			return
+		}
+
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		input := m.Content[1:]
+		response := geminiapi.GeneratePrompt(input)
+
+		_, err := s.ChannelMessageSend(m.ChannelID, response)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	session.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = session.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(session *discordgo.Session) {
+		err := session.Close()
+		if err != nil {
+		}
+	}(session)
+	
+	fmt.Println("Traduz.ai is running")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
